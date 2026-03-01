d4c72b0c7498c2342f755e57d9bbefb2fa81b88d
Cai <caiowain@gmail.com>
2026-03-01 16:26:23 +0100

    revert

--- backend/src/main/java/com/cadfancode/Backend/config/WebConfig.java
+++ backend/src/main/java/com/cadfancode/Backend/config/WebConfig.java
@@ -1,22 +1,21 @@
 package com.cadfancode.Backend.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "http://localhost:5174",
-                        "https://filter-usurping-candidate-method.vercel.app"
-                )
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**") // Apply to all endpoints
+                    .allowedOrigins(
+                            "http://localhost:5174", // Vue local development port
+                            "https://filter-usurping-candidate-method.vercel.app/"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
     }
-}
+
--- backend/src/main/java/com/cadfancode/Backend/repository/SearchSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cadfancode.Backend.repository;
-
-import com.cadfancode.Backend.model.SearchSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SearchSessionRepository extends JpaRepository<SearchSession, String> {
-}
